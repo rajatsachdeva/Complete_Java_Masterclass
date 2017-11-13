772bc3b57cde75c6970159409a1a475156e7bcc7
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-13 16:50:22 +0530

    added some more
    Primitve datatypes

--- 4-Variables_Datatypes_and_Operators/17-Getting_To_Know-Primitive_Data_Types/src/com/company/Main.java
+++ 4-Variables_Datatypes_and_Operators/17-Getting_To_Know-Primitive_Data_Types/src/com/company/Main.java
@@ -4,6 +4,8 @@
 
 package com.company;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -26,22 +28,44 @@ public class Main {
         int myMaxValue      = 2_147_483_647;
 
         /* Size/width of Byte = 1 byte , 8 bits , -128 to 127" */
-        byte myByteValue    = 10;
+        byte myByteValue    = -128;
+        //byte myNewByteValue = (myByteValue/2); Gives an error as 2 is not Byte it's Integer
+        //Type casting
+        byte myNewByteValue = (byte) (myByteValue/2);
 
         /* Size/width of short = 2 bytes, 16 bits
         * -32768 to 32767
         */
-        short myShortValue  = 30000;
+        short myShortValue  = 32767;
+        short myNewShortValue = (short)(myShortValue/2);
 
         /* We can use l/L , it's better to use L for better readability
          * Long has a width of 64 bits (8 byte)
          */
         long myLongValue    = 100L;
 
-        System.out.println(myMinValue);
-        System.out.println(myMaxValue);
-        System.out.println(myByteValue);
-        System.out.println(myShortValue);
-        System.out.println(myLongValue);
+        System.out.println("myMinValue: "   + myMinValue);
+        System.out.println("myMaxValue: "   + myMaxValue);
+        System.out.println("myByteValue: "  + myByteValue);
+        System.out.println("myShortValue: " + myShortValue);
+        System.out.println("myLongValue: "  + myLongValue);
+
+        int myTotal         = (myMinValue/2);
+        System.out.println("myTotal: "       + myTotal);
+        System.out.println("myNewByteValue: " +  myNewByteValue);
+        System.out.println("myNewShortValue: " + myNewShortValue);
+
+        // Create a Byte variable
+        byte myByte = 10;
+
+        // Create a short variable
+        short myShort = 20000;
+
+        // Create an int variable
+        int myInt = 4000000;
+
+        // Create a long variable
+        long myLong = (long) (50000 + (10 * (myByte + myShort + myInt)));
+        System.out.println("myLong: " + myLong);
     }
 }
