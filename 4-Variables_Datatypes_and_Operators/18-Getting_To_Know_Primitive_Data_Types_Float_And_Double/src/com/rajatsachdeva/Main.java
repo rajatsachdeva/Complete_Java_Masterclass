c707b10276cf332a983f3335dfd8bff8d7fb697d
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-14 23:15:53 +0530

    auto indent

--- 4-Variables_Datatypes_and_Operators/18-Getting_To_Know_Primitive_Data_Types_Float_And_Double/src/com/rajatsachdeva/Main.java
+++ 4-Variables_Datatypes_and_Operators/18-Getting_To_Know_Primitive_Data_Types_Float_And_Double/src/com/rajatsachdeva/Main.java
@@ -14,9 +14,9 @@ public class Main {
         /* Declarations */
 
         // Width of integer = 4 bytes
-        int     myIntValue      = 5;
+        int myIntValue = 5;
         // Width of float  = 4 bytes
-        float   myFloatValue    = 5f;
+        float myFloatValue = 5f;
         //myFloatValue            = (float) 5.25;
         // Width of double  = 8 bytes
         /*
@@ -24,23 +24,23 @@ public class Main {
          * calculations as: 1) it's fast and used in most machines
          * 2) Used in most predefined java methods
          */
-        double  myDoubleValue   = 5d;
+        double myDoubleValue = 5d;
 
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("myFloatValue = " + myFloatValue);
         System.out.println("myDoubleValue = " + myDoubleValue);
 
-        myIntValue              = 5 / 2;
-        myFloatValue            = 5f / 2;
-        myDoubleValue           = 5d / 2;
+        myIntValue = 5 / 2;
+        myFloatValue = 5f / 2;
+        myDoubleValue = 5d / 2;
 
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("myFloatValue = " + myFloatValue);
         System.out.println("myDoubleValue = " + myDoubleValue);
 
-        myIntValue              = 5 / 3;
-        myFloatValue            = 5f / 3f;
-        myDoubleValue           = 5d / 3d;
+        myIntValue = 5 / 3;
+        myFloatValue = 5f / 3f;
+        myDoubleValue = 5d / 3d;
 
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("myFloatValue = " + myFloatValue);
@@ -57,8 +57,8 @@ public class Main {
         */
 
         double myPounds = 200d;
-        double mykg     = myPounds * 0.45359237d;   // 90.7185
-        System.out.println("Pounds = " + myPounds + "\nkilograms = " + mykg );
+        double mykg = myPounds * 0.45359237d;   // 90.7185
+        System.out.println("Pounds = " + myPounds + "\nkilograms = " + mykg);
 
         // pi value
         double pi = 3.141_592_7d;
