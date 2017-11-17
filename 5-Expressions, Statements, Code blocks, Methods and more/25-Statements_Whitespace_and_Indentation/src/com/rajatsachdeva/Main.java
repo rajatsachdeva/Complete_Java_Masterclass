c68de5d329238f85347f4c4371f033258fc184f7
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-18 00:27:47 +0530

    added 25

--- 5-Expressions, Statements, Code blocks, Methods and more/25-Statements_Whitespace_and_Indentation/src/com/rajatsachdeva/Main.java
+++ 5-Expressions, Statements, Code blocks, Methods and more/25-Statements_Whitespace_and_Indentation/src/com/rajatsachdeva/Main.java
@@ -3,6 +3,22 @@ package com.rajatsachdeva;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Statements
+	    int myVariable = 50;
+
+	    if(myVariable == 50)
+            System.out.println("Printed");
+
+	    myVariable++;
+	    myVariable--;
+
+        System.out.println("This is a test");
+        System.out.println("This is" +
+            " another" + " still more.");
+
+        int anotherVariable = 50;myVariable++;System.out.println("This is another one");
+        anotherVariable--;
+
+
     }
 }
