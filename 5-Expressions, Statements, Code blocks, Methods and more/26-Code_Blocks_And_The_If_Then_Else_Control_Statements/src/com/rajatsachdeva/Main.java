fba410443229e8f1fe3a72b02d5f73db214af9c6
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-18 00:37:01 +0530

    added 26

---
+++ 5-Expressions, Statements, Code blocks, Methods and more/26-Code_Blocks_And_The_If_Then_Else_Control_Statements/src/com/rajatsachdeva/Main.java
@@ -0,0 +1,24 @@
+package com.rajatsachdeva;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score == 5000) {
+            System.out.println("Your score was 5000");
+            System.out.println("This was executed");
+        }
+        System.out.println("This gets executed anyways !");
+
+        score = 1000;
+        if(score < 5000) {
+            System.out.println("Your score was less than 5000");
+        } else {
+            System.out.println("Got here");
+        }
+    }
+}
