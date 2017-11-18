b9efabba3ab48b9c66b9ded3fac0ef50106819ea
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-18 13:39:42 +0530

    updated 26

--- 5-Expressions, Statements, Code blocks, Methods and more/26-Code_Blocks_And_The_If_Then_Else_Control_Statements/src/com/rajatsachdeva/Main.java
+++ 5-Expressions, Statements, Code blocks, Methods and more/26-Code_Blocks_And_The_If_Then_Else_Control_Statements/src/com/rajatsachdeva/Main.java
@@ -15,10 +15,44 @@ public class Main {
         System.out.println("This gets executed anyways !");
 
         score = 1000;
-        if(score < 5000) {
+        if (score < 5000) {
             System.out.println("Your score was less than 5000");
         } else {
             System.out.println("Got here");
+        }
+
+        score = 6000;
+        if(score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000 but "
+            + "greater than 1000");
+        } else if (score < 1000){
+            System.out.println("Your score was less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
+
+        score = 800;
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+        // cannot access finalScore as it's out of scope
+        //int savedFinalScore = finalScore;
+
+        //Challenge
+        /*
+         * Print out a score score on the screen with the following
+         * score set to 10000
+         * levelCompleted set to 8
+         * bonus set to 200
+         * but make sure the first printout still displays as well
+         */
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your second final score was " + finalScore);
         }
     }
 }
