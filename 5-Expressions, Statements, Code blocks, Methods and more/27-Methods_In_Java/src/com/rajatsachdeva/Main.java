a9e9da9cbcbf76a678f1d0ec9aa56a7922faa35d
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-18 13:55:53 +0530

    updated 27

--- 5-Expressions, Statements, Code blocks, Methods and more/27-Methods_In_Java/src/com/rajatsachdeva/Main.java
+++ 5-Expressions, Statements, Code blocks, Methods and more/27-Methods_In_Java/src/com/rajatsachdeva/Main.java
@@ -2,7 +2,26 @@ package com.rajatsachdeva;
 
 public class Main {
 
+    // main method
     public static void main(String[] args) {
-	// write your code here
+	    boolean gameOver = true;
+	    int score = 800;
+	    int levelCompleted = 5;
+	    int bonus = 100;
+
+        /* First call */
+	    calculateScore(true, 800, 5, 100);
+	    /* Second Call */
+        calculateScore(true,10000, 8, 200 );
+    }
+
+    // calculateScore method
+    public static void calculateScore (boolean gameOver, int score,
+                                       int levelCompleted, int bonus) {
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore );
+        }
     }
 }
