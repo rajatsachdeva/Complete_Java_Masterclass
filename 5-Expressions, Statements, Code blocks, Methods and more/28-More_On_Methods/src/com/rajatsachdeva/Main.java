8032f93cc9d3f6fd1798540fc47cb7a528f4d446
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-19 21:18:04 +0530

    added 28 challenge

--- 5-Expressions, Statements, Code blocks, Methods and more/28-More_On_Methods/src/com/rajatsachdeva/Main.java
+++ 5-Expressions, Statements, Code blocks, Methods and more/28-More_On_Methods/src/com/rajatsachdeva/Main.java
@@ -45,6 +45,44 @@ public class Main {
         call both methods and display the results of the following
         a score of 1500, 900, 400 and 50
         */
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Rajat", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Rohan", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Bob",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Carol", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Louise", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Percy", highScorePosition);
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " +
+        highScorePosition + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 
     // calculateScore method
