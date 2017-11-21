d4de452af2f4201dec6faaa483266864e845eb54
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-21 22:04:29 +0530

    update 32 - added the prime number challenge

--- 6-Control_Flow_Statements/32-The_for_Statement/src/com/rajatsachdeva/Main.java
+++ 6-Control_Flow_Statements/32-The_for_Statement/src/com/rajatsachdeva/Main.java
@@ -35,6 +35,41 @@ public class Main {
             System.out.println("10,000 at " + i + "% interest = "
                     + String.format("%.2f", calculateInterest(10000.0, i)));
         }
+
+        int numberOfPrime = 0;
+        for (int i = 600; i <= 700; i++) {
+            //Check if prime
+            if(isPrime(i)) {
+                System.out.println(i + " is a Prime");
+                numberOfPrime++;
+                if(numberOfPrime == 3) {
+                    break;
+                }
+            }
+        }
+    }
+
+    // Create a for statement using any range of numbers
+    // Determine if the number is a prime number using the isPrime method
+    // if it is a prime number, print it out AND increment a count of the
+    // number of prime numbers found
+    // if that count is 3 exit the for loop
+    // hint:  Use the break; statement to exit
+
+    // If Prime or not
+    public static boolean isPrime(int n) {
+
+        if(n == 1) {
+            return false;
+        }
+
+        for(int i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     // Simple Interest
