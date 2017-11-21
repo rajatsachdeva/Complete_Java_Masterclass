7a2aa59f78b8b9e2e20545011d248f5f20ca1e86
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-21 22:45:03 +0530

    completed 33
    while and do while

--- 6-Control_Flow_Statements/33-The_while_and_do_while_statements/src/com/rajatsachdeva/Main.java
+++ 6-Control_Flow_Statements/33-The_while_and_do_while_statements/src/com/rajatsachdeva/Main.java
@@ -3,6 +3,73 @@ package com.rajatsachdeva;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int count = 1;
+        while (count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        count = 1;
+        System.out.println("\nwhile: Entry check loop");
+        while (true) {
+            if (count == 6) {
+                System.out.println("Breaking from loop on " + count + " count");
+                break;
+            }
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        System.out.println("\ndo while: Exit check loop");
+        count = 6;
+        // Always going to execute once
+        do {
+            System.out.println("Count value was " + count);
+            count++;
+
+            if (count > 100) {
+                break;
+            }
+        } while (count != 6);
+
+        System.out.println("\nIs " + count + " Prime: " + isEvenNumber(count) + "\n");
+        int number = 5;
+        int finishNumber = 20;
+        while (number <= finishNumber) {
+            if (!isEvenNumber(number)) {
+                number++;
+                continue;
+            }
+            System.out.println("Even number " + number);
+            number++;
+        }
+
+        // Modify the while code above
+        // Make it also record the total number of even numbers it has found
+        // and break once 5 are found
+        // and at the end, display the total number of even numbers found
+        number = 5;
+        finishNumber = 20;
+        System.out.println();
+        int numberOfEven = 0;
+        while (number < finishNumber && numberOfEven != 5) {
+            if (isEvenNumber(number)) {
+                System.out.println("Even number " + number);
+                numberOfEven++;
+            }
+            number++;
+        }
+        System.out.println("Total number of even numbers = " + numberOfEven);
+    }
+
+    // Create a method called isEvenNumber that takes a parameter of type int
+    // Its purpose is to determine if the argument passed to the method is
+    // an even number or not.
+    // return true if an even number, otherwise return false;
+    public static boolean isEvenNumber(int num) {
+        if (num % 2 == 0) {
+            return true;
+        }
+        return false;
     }
 }
