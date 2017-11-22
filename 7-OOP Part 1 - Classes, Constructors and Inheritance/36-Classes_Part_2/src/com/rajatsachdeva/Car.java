2fc65f8bdf5fd29b9a413717b6616eb3fac15c5e
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-23 00:05:23 +0530

    completed 36

--- 7-OOP Part 1 - Classes, Constructors and Inheritance/36-Classes_Part_2/src/com/rajatsachdeva/Car.java
+++ 7-OOP Part 1 - Classes, Constructors and Inheritance/36-Classes_Part_2/src/com/rajatsachdeva/Car.java
@@ -9,11 +9,48 @@ public class Car {
 
     //setter
     public void setModel(String model) {
-        this.model = model;
+        String validModel = model.toLowerCase();
+        if(validModel.equals("caymans") || validModel.equals("civic")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
     }
 
     //getter
     public String getModel() {
         return this.model;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
     }
 }
--- 7-OOP Part 1 - Classes, Constructors and Inheritance/36-Classes_Part_2/src/com/rajatsachdeva/Main.java
+++ 7-OOP Part 1 - Classes, Constructors and Inheritance/36-Classes_Part_2/src/com/rajatsachdeva/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Car porsche = new Car();
-        Car holden = new Car();
+        Car honda = new Car();
+
         // porsche = null; gives null pointer exception
-        System.out.println("Model is " + porsche.getModel());
+
+        System.out.println("Model is " + porsche.getModel()); // gives null
         porsche.setModel("CaymanS");
         System.out.println("Model is " + porsche.getModel());
+
+        honda.setModel("City");
+        System.out.println("Model is " + honda.getModel());
     }
 }
