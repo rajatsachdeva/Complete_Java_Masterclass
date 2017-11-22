1c0e10469331d032e37d8530b87c861e617563a8
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-22 22:16:25 +0530

    Started Classes Part 1
    - Created class Car

---
+++ 7-OOP Part 1 - Classes, Constructors and Inheritance/34-35-Classes_Part_1/src/com/rajatsachdeva/Car.java
@@ -0,0 +1,7 @@
+package com.rajatsachdeva;
+
+//with public scope other classes have access to this class
+
+public class Car {
+
+}
---
+++ 7-OOP Part 1 - Classes, Constructors and Inheritance/34-35-Classes_Part_1/src/com/rajatsachdeva/Main.java
@@ -0,0 +1,15 @@
+package com.rajatsachdeva;
+
+// Creates Main Class
+public class Main {
+
+    public static void main(String[] args) {
+        // Objects are all around us
+        // Objects have two things: State and Behaviour
+        // State: Fields/ Variables
+        // Behaviour: Methods to express
+        // Classes: Template/ Blue Print for creating Objects
+        // User defined Type ( A powerful one )
+
+    }
+}
