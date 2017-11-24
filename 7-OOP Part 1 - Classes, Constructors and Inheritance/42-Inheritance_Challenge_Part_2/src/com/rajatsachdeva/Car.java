3e266830fe6726af376fd3bca24df7d730afd43f
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-25 00:44:23 +0530

    Started 42
    Inheritance challenge part 2

---
+++ 7-OOP Part 1 - Classes, Constructors and Inheritance/42-Inheritance_Challenge_Part_2/src/com/rajatsachdeva/Car.java
@@ -0,0 +1,35 @@
+package com.rajatsachdeva;
+
+public class Car extends Vehicle {
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean isManual;
+
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManual) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = 1; // First Gear
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Cat.setCurrentGear(): Changed to " + this.currentGear + " gear");
+    }
+
+    public void changeVelocity(int speed, int direction) {
+        System.out.println("Car.changeVelocity() called");
+        move(speed, direction);
+        System.out.println("Car.changeVelocity(): Velocity " + speed + " direction " + direction);
+    }
+
+//    @Override
+//    public void stop() {
+//        super.stop();
+//    }
+}
