eca7ccd18e0b541b75cdf7a91066dd1848bf4b75
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-11-25 14:17:51 +0530

    Started 45
    Composition Part 2

---
+++ 8-OOP Part 2 - Composition, Encapsulation, and Polymorphism/45-Composition_Part2/src/com/rajatsachdeva/Car.java
@@ -0,0 +1,13 @@
+package com.rajatsachdeva;
+
+// Car is a Vehicle
+public class Car extends Vehicle {
+    private int doors;
+    private int engineCapacity;
+
+    public Car(String name, int doors, int engineCapacity) {
+        super(name);
+        this.doors = doors;
+        this.engineCapacity = engineCapacity;
+    }
+}
