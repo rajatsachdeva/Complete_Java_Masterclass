66193ccea217ff30c73f3471d4b7174e78b320d3
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-02 14:08:52 +0530

    added 53 resizing array

--- 9-Arrays, Java inbuilt Lists, Autoboxing and Unboxing/54-List_and_ArrayList_Part_1/src/com/rajatsachdeva/Main.java
+++ 9-Arrays, Java inbuilt Lists, Autoboxing and Unboxing/54-List_and_ArrayList_Part_1/src/com/rajatsachdeva/Main.java
@@ -1,8 +1,40 @@
 package com.rajatsachdeva;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static int[] baseData = new int[10];
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Enter 10 Integers: ");
+        getInput();
+        printArray(baseData);
+        resizeArray();
+        System.out.println("Enter 12 integers: ");
+        getInput();
+        printArray(baseData);
+    }
+
+    public static void getInput() {
+        for (int i = 0; i < baseData.length; i++) {
+            baseData[i] = scanner.nextInt();
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void resizeArray() {
+        int[] original = baseData;
+        baseData = new int[12];
+        for (int i = 0; i < original.length; i++) {
+            baseData[i] = original[i];
+        }
     }
 }
