0da82e4b915a353a17b407f0efde70f716b73ff2
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-05 21:53:09 +0530

    Completed 64
    Linked List

--- 9-Arrays, Java inbuilt Lists, Autoboxing and Unboxing/64_65_66-LinkedList_Part_1_2_3/src/com/rajatsachdeva/Main.java
+++ 9-Arrays, Java inbuilt Lists, Autoboxing and Unboxing/64_65_66-LinkedList_Part_1_2_3/src/com/rajatsachdeva/Main.java
@@ -1,8 +1,32 @@
 package com.rajatsachdeva;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Customer customer = new Customer("Rajat", 100.00);
+        Customer anotherCustomer;
+        anotherCustomer = customer; // reference to customer object
+        anotherCustomer.setBalance(120.00);
+
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for(int i = 0; i < intList.size(); i++) {
+            System.out.println(i + " : " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+        System.out.println();
+        for(int i = 0; i < intList.size(); i++) {
+            System.out.println(i + " : " + intList.get(i));
+        }
     }
 }
