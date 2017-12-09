a06b286998f7161ec8a44a3031722b030e30717d
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-09 23:53:03 +0530

    Started 70
    Interfaces, added grearbox class

--- 10-Inner and Abstract Classes & Interfaces/70-Interfaces/src/com/rajatsachdeva/Gearbox.java
+++ 10-Inner and Abstract Classes & Interfaces/70-Interfaces/src/com/rajatsachdeva/Gearbox.java
@@ -1,4 +1,10 @@
 package com.rajatsachdeva;
 
 public class Gearbox {
+
+    private boolean clutchIsIn;
+
+    public void operateClutch(boolean inOrOut) {
+        this.clutchIsIn = inOrOut;
+    }
 }
