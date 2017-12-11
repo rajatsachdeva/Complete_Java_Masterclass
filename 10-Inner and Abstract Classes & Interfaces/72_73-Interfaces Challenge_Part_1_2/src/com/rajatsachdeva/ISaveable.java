d5270920edf8555ec0fb2d6d65fb2f89887f8b2a
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-11 22:46:56 +0530

    Completed 72 Interface Challenge

---
+++ 10-Inner and Abstract Classes & Interfaces/72_73-Interfaces Challenge_Part_1_2/src/com/rajatsachdeva/ISaveable.java
@@ -0,0 +1,9 @@
+package com.rajatsachdeva;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ISaveable {
+    List<String> write();
+    void read(List<String> savedValues);
+}
--- 10-Inner and Abstract Classes & Interfaces/72_73-Interfaces Challenge_Part_1_2/src/com/rajatsachdeva/Main.java
+++ 10-Inner and Abstract Classes & Interfaces/72_73-Interfaces Challenge_Part_1_2/src/com/rajatsachdeva/Main.java
@@ -19,9 +19,53 @@ package com.rajatsachdeva;
 // simulate getting values from a file – this allows you to type as many values as your class
 // requires, and returns an ArrayList.
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    System.out.println("Quiting");
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISaveable objectToSave) {
+        for (int i = 0; i < objectToSave.write().size(); i++) {
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+        }
+    }
+
+    public static void loadObject (ISaveable obecjtToLoad) {
+        ArrayList<String> values = readValues();
+        obecjtToLoad.read(values);
     }
 }
