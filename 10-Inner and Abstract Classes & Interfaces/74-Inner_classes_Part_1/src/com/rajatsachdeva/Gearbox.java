a886a39a35217015a6b37b9098c98ef49f01dc55
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-12 22:50:15 +0530

    Completed 74 Inner Classes Part 1

--- 10-Inner and Abstract Classes & Interfaces/74-Inner_classes_Part_1/src/com/rajatsachdeva/Gearbox.java
+++ 10-Inner and Abstract Classes & Interfaces/74-Inner_classes_Part_1/src/com/rajatsachdeva/Gearbox.java
@@ -6,12 +6,41 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
         this.gears = new ArrayList<Gear>();
         Gear neutral = new Gear(0, 0.0);
         this.gears.add(neutral);
+    }
+
+    public void operateClutch(boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio) {
+        if ((number > 0) && (number <= maxGears)) {
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear) {
+        if ((newGear >= 0) && (newGear < this.gears.size()) && this.clutchIsIn) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected.");
+        } else {
+            System.out.println("Grind !");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs) {
+        if (clutchIsIn) {
+            System.out.println("Scream !");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
     }
 
     public class Gear {
@@ -25,6 +54,10 @@ public class Gearbox {
 
         public double driveSpeed(int revs) {
             return revs * (this.ratio);
+        }
+
+        public double getRatio() {
+            return ratio;
         }
     }
 }
