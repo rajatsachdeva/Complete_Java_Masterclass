5a296199ffabef720e2c9854efe2a929e5f96d54
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-16 00:43:48 +0530

    Started 79 Added abstract class ListItem
    Abstract Class Challenge

---
+++ 10-Inner and Abstract Classes & Interfaces/79_80_81-Abstract_Class_Challenge_Part_1_2_3/src/com/rajatsachdeva/ListItem.java
@@ -0,0 +1,31 @@
+package com.rajatsachdeva;
+
+public abstract class ListItem {
+
+    // Self referential
+    protected ListItem rightLink = null;
+    protected ListItem leftLink = null;
+
+    // To store any type of object
+    public Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+
+    abstract ListItem next();
+    abstract ListItem setNext(ListItem item);
+
+    abstract ListItem previous();
+    abstract ListItem SetPrevious(ListItem item);
+
+    abstract int compareTo(ListItem item);
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
