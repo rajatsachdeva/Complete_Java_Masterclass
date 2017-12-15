700dfd1103939887f9d3549cfc4b97b853061b41
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-15 22:27:46 +0530

    Started 77 , Added animal abstract class
    Abstract Classes Part 1

--- 10-Inner and Abstract Classes & Interfaces/77-Abstract_Classes_Part_1/src/com/rajatsachdeva/Main.java
+++ 10-Inner and Abstract Classes & Interfaces/77-Abstract_Classes_Part_1/src/com/rajatsachdeva/Main.java
@@ -1,8 +1,18 @@
 package com.rajatsachdeva;
 
+/*
+    Abstract Class/Method
+    Meaning Something that is only declared (What needs to be done ?)
+    and not defined (How it needs to be done ?)
+
+    Interfaces are abstract
+
+    Abstract Class cannot be Instantiated
+*/
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
     }
 }
