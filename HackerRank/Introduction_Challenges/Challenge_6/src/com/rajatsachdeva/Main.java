e17b38f154bae957f256eac4bf364c8a549f67f8
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-17 02:12:39 +0530

    Completed challenge 5 and 6

---
+++ HackerRank/Introduction_Challenges/Challenge_6/src/com/rajatsachdeva/Main.java
@@ -0,0 +1,53 @@
+package com.rajatsachdeva;
+
+// Java Loops I
+
+import java.util.Scanner;
+
+/**
+ * Objective
+ In this challenge, we're going to use loops to help us do some simple math.
+
+ Task
+ Given an integer, , print its first 10 multiples. Each multiple  (where ) should be printed on a new line in the
+ form: N x i = result.
+
+ Input Format
+
+ A single integer, .
+
+ Constraints
+
+
+ Output Format
+
+ Print  lines of output; each line  (where ) contains the  of  in the form:
+ N x i = result.
+
+ Sample Input
+
+ 2
+ Sample Output
+
+ 2 x 1 = 2
+ 2 x 2 = 4
+ 2 x 3 = 6
+ 2 x 4 = 8
+ 2 x 5 = 10
+ 2 x 6 = 12
+ 2 x 7 = 14
+ 2 x 8 = 16
+ 2 x 9 = 18
+ 2 x 10 = 20
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + (N*i));
+        }
+    }
+}
