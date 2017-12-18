7c08319c7d1f3e00e9a1d50053a82a61186fc998
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-18 22:35:08 +0530

    Completed challenges 7, 8, and 9 updated readme.md added steps to run java program from CLI

--- HackerRank/Introduction_Challenges/Challenge_9/src/com/rajatsachdeva/Main.java
+++ HackerRank/Introduction_Challenges/Challenge_9/src/com/rajatsachdeva/Main.java
@@ -5,14 +5,15 @@ package com.rajatsachdeva;
 import java.util.Scanner;
 
 /**
- * "In computing, End Of File (commonly abbreviated EOF) is a condition in a computer operating system where no more data can be read from a data source." — (Wikipedia: End-of-file)
+ * "In computing, End Of File (commonly abbreviated EOF) is a condition in a computer operating system where no more
+ * data can be read from a data source." — (Wikipedia: End-of-file)
  The challenge here is to read  lines of input until you reach EOF, then number and print all  lines of content.
 
  Hint: Java's Scanner.hasNext() method is helpful for this problem.
 
  Input Format
 
- Read some unknown  lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
+ Read some unknown  lines of input from stdin(System.in) until you reach EOF; each line of input
 
  Output Format
 
