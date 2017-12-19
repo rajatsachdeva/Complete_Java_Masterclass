9dcd4364dc6ef6f0162bc843623d1b83b6880a06
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-19 22:47:22 +0530

    Completed 83, Started 84 Generic Class

--- 11-Java Generics/83_84_85-Our-Generics-Class_1_2_3/src/com/rajatsachdeva/Main.java
+++ 11-Java Generics/83_84_85-Our-Generics-Class_1_2_3/src/com/rajatsachdeva/Main.java
@@ -3,6 +3,16 @@ package com.rajatsachdeva;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer messi = new SoccerPlayer("messi");
+
+        Team teamStark = new Team("Team Stark");
+        teamStark.addPlayer(joe);
+        teamStark.addPlayer(pat);
+        teamStark.addPlayer(messi);
+
+        System.out.println(teamStark.numPlayers());
+
     }
 }
