665fd3e92b13aa49f45311d2cf790c553db9d333
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-19 22:52:29 +0530

    Completed 83, Started 84 Generic Class

--- 11-Java Generics/83_84_85-Our-Generics-Class_1_2_3/src/com/rajatsachdeva/Main.java
+++ 11-Java Generics/83_84_85-Our-Generics-Class_1_2_3/src/com/rajatsachdeva/Main.java
@@ -5,14 +5,15 @@ public class Main {
     public static void main(String[] args) {
         FootballPlayer joe = new FootballPlayer("Joe");
         BaseballPlayer pat = new BaseballPlayer("Pat");
-        SoccerPlayer messi = new SoccerPlayer("messi");
+        SoccerPlayer messi = new SoccerPlayer("Messi");
 
         Team teamStark = new Team("Team Stark");
-        teamStark.addPlayer(joe);
-        teamStark.addPlayer(pat);
-        teamStark.addPlayer(messi);
+        // Our team class is allowing us to add any player type to same team
+        // There is no actual to see if players being added are of same type
+        teamStark.addPlayer(joe); // Football player
+        teamStark.addPlayer(pat); // BaseBall player
+        teamStark.addPlayer(messi); // Soccer Player
 
         System.out.println(teamStark.numPlayers());
-
     }
 }
