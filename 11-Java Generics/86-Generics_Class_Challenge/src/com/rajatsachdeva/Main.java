9d9d4a80a7e09130a29fda8c4b0e3dbac766d107
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-20 23:30:30 +0530

    Started 86 Generic Class Challenge

---
+++ 11-Java Generics/86-Generics_Class_Challenge/src/com/rajatsachdeva/Main.java
@@ -0,0 +1,24 @@
+package com.rajatsachdeva;
+
+// Create a generic class to implement a league table for a sport.
+// The class should allow teams to be added to the list, and store
+// a list of teams that belong to the league.
+//
+// Your class should have a method to print out the teams in order,
+// with the team at the top of the league printed first.
+//
+// Only teams of the same type should be added to any particular
+// instance of the league class - the program should fail to compile
+// if an attempt is made to add an incompatible team.
+//
+// ArrayList<Team> teams;
+// Collections.sort(teams);
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
