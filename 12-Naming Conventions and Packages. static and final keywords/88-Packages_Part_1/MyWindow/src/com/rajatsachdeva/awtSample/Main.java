33e0a2dec731c311d032bfe9418b57647357c509
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-27 22:59:22 +0530

    Completed 88

---
+++ 12-Naming Conventions and Packages. static and final keywords/88-Packages_Part_1/MyWindow/src/com/rajatsachdeva/awtSample/Main.java
@@ -0,0 +1,9 @@
+package com.rajatsachdeva.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    MyWindow myWindow = new MyWindow("Complete Java");
+	    myWindow.setVisible(true);
+    }
+}
