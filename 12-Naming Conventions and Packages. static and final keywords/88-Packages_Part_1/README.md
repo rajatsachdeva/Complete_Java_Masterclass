5061b8a56d8838eb68b248799146236094ebbd19
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-27 22:03:13 +0530

    Started 88 Packages in Java

---
+++ 12-Naming Conventions and Packages. static and final keywords/88-Packages_Part_1/README.md
@@ -0,0 +1,9 @@
+# Lecture 88 - Packages
+
+## Packages <a name="packages">
+> Over 9 million Java developers worldwide
+> Class or interface name conflicts are inevitable
+> Mechanism is need to fully specify class
+> Allow use of classes with the same name in the same project (or, even, the same class)
+> Package is a way of _**grouping**_ related classes and interfaces together
+> Package mechanism provides a way to manage the _namepace_ of object types and it also extends access protection
---
+++ 12-Naming Conventions and Packages. static and final keywords/88-Packages_Part_1/src/com/rajatsachdeva/Main.java
@@ -0,0 +1,22 @@
+package com.rajatsachdeva;
+
+// Packages
+
+import javafx.scene.Node;
+// import org.w3c.dom.Node; // won't compile
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // whole package is not imported, so we give the actual whole path or full name for class etc.
+        // But this will get tiresome
+        // Also if we want 2 instances of the Node class then we can't do 2 imports , It won't compile
+        // so we may have to use the following type of declaration
+        javax.xml.soap.Node node = null;
+        org.w3c.dom.Node anotherNode = null;
+
+        Node thirdNode = null; // will take import package Node type, and will work
+
+    }
+}
