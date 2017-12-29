d11b259c2c95b3998e35c8fbdf8a2b05144aece4
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-12-29 11:03:01 +0530

    Completed Lecture 90 Packages Part 3

---
+++ 12-Naming Conventions and Packages. static and final keywords/89-Packages_Part_2/src/com/example/game/ISaveable.java
@@ -0,0 +1,9 @@
+package com.example.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ISaveable {
+    List<String> write();
+    void read(List<String> savedValues);
+}
---
+++ 12-Naming Conventions and Packages. static and final keywords/89-Packages_Part_2/src/com/example/game/Monster.java
@@ -0,0 +1,68 @@
+package com.example.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    @Override
+    public List<String> write() {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(1, "" + this.strength);
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if (null != savedValues && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.strength = Integer.parseInt(savedValues.get(1));
+            this.hitPoints = Integer.parseInt(savedValues.get(2));
+        }
+
+    }
+}
