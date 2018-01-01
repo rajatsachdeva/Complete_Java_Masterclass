8bedb7af2f044d78a28fd4054f7c0a7d16b0229f
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-01-01 22:35:04 +0530

    In Progress 97 the final statement added readme notes

--- 12-Naming Conventions and Packages. static and final keywords/97-The_final_statement/README.md
+++ 12-Naming Conventions and Packages. static and final keywords/97-The_final_statement/README.md
@@ -1,3 +1,6 @@
 # Lecture 97 - The final Statement
 
-> 
+> _final_ keyword is usually used to define constant values <br />
+> _final_ fields are _not_ actually constants, as they can be modified but only _**once**_, any modification must be 
+performed before the class constructor finishes, i.e. while declaring the field or in constructor <br />
+>  
--- 12-Naming Conventions and Packages. static and final keywords/97-The_final_statement/src/com/rajatsachdeva/Main.java
+++ 12-Naming Conventions and Packages. static and final keywords/97-The_final_statement/src/com/rajatsachdeva/Main.java
@@ -3,6 +3,18 @@ package com.rajatsachdeva;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SomeClass one = new SomeClass("one");
+        System.out.println("Class Counter = " + SomeClass.getClassCounter());
+        SomeClass two = new SomeClass("two");
+        System.out.println("Class Counter = " + SomeClass.getClassCounter());
+        SomeClass three = new SomeClass("three");
+        System.out.println("Class Counter = " + SomeClass.getClassCounter());
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+        // Cannot assign value to final variable
+        // one.instanceNumber = 5;
     }
 }
