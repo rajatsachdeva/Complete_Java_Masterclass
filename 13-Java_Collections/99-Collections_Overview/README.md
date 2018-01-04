00bee4c2b9ed51cac466e1c0f2bb3ca7de563c7f
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-01-04 23:37:13 +0530

    Completed 99 Collections Overview

--- 13-Java_Collections/99-Collections_Overview/README.md
+++ 13-Java_Collections/99-Collections_Overview/README.md
@@ -1,1 +1,3 @@
 # Lecture 99 - Collections Overview
+
+The core elements of the collections framework are interfaces including the list interface etc. <br />
--- 13-Java_Collections/99-Collections_Overview/src/com/rajatsachdeva/Main.java
+++ 13-Java_Collections/99-Collections_Overview/src/com/rajatsachdeva/Main.java
@@ -3,6 +3,19 @@ package com.rajatsachdeva;
 public class Main {
 
     public static void main(String[] args) {
+        Theatre theatre = new Theatre("IMAX", 8, 12);
+        //theatre.getSeats();
 
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
     }
 }
