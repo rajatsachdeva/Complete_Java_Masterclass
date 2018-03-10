00dbc4beda5009f9c83611fdb0ac9b52b0bb705e
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-03-11 01:34:13 +0530

    Lecture 176 Finished
    Section 15

--- 15-Basic-Input-&-Output-including-java/176-Stack_Trace_and_Call_Stack/README.md
+++ 15-Basic-Input-&-Output-including-java/176-Stack_Trace_and_Call_Stack/README.md
@@ -8,3 +8,10 @@ a. what went wrong ?
 b. what can we do about it ?
 
 exception and runtime exception are classes defined in Java.lang
+
+Call Stack is a list of all the methods the method calls at any particular point in a program
+execution
+
+In Case of Exception(where a program crashes) A call stack in printed as well with Exception message
+
+So Each thread of execution has its own call stack and the thread is shown as first line of call stack 
--- 15-Basic-Input-&-Output-including-java/176-Stack_Trace_and_Call_Stack/src/com/rajatsachdeva/Main.java
+++ 15-Basic-Input-&-Output-including-java/176-Stack_Trace_and_Call_Stack/src/com/rajatsachdeva/Main.java
@@ -1,8 +1,94 @@
 package com.rajatsachdeva;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /**
+         * LBYL: Look Before You Leap
+         *
+         * EAFP: Easy to Ask for Forgiveness than Permission
+         */
+
+        int x = 98;
+        int y = 0;
+
+        System.out.println(divideLBYL(x, y));
+        System.out.println(divideEAFP(x, y));
+        //System.out.println(divide(x, y));
+
+        //int z = getInt();
+        //System.out.println("z is " + z);
+
+//        int z = getIntLBYL();
+        int z = getIntEAFP();
+        System.out.println("z is " + z);
+    }
+
+    private static int getInt() {
+        System.out.print("Enter: ");
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+
+    // Doesn't seem very efficient
+    private static int getIntLBYL() {
+        Scanner s = new Scanner(System.in);
+        boolean isValid = true;
+        System.out.println("Please enter an integer ");
+        String input = s.next();
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid) {
+            return Integer.parseInt(input);
+        }
+
+        return 0;
+    }
+
+    private static int getIntEAFP() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter an Integer");
+
+        try {
+            return s.nextInt();
+        } catch (InputMismatchException e) {
+            return 0;
+        }
+    }
+    private static int divideLBYL(int x, int y) {
+        if (y != 0) {
+            return x / y;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int divideEAFP(int x, int y) {
+        try {
+            return x / y;
+        } catch(ArithmeticException e) {
+            System.out.println(e);
+            return 0;
+        }
+    }
+
+    /**
+     * divide
+     *
+     * Will generate the Arithmetic Exception in case of divide by 0
+     * and hence crash
+     * @param x
+     * @param y
+     * @return x / y
+     */
+    private static int divide(int x, int y) {
+        return x / y;
     }
 }
