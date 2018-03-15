08668dd43dc1a7fb6163235b660af0f07150e689
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-03-16 00:10:57 +0530

    Lecture 104 Section 13

---
+++ 13-Java_Collections/104-Map_Continued_and_Adventure_Game/src/com/rajatsachdeva/Main.java
@@ -0,0 +1,45 @@
+package com.rajatsachdeva;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("IMAX", 8, 12);
+
+        if (theatre.reserveSeat("D12")) {
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat already taken");
+        }
+
+        if (theatre.reserveSeat("B13")) {
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat already taken");
+        }
+
+        List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeats);
+        printList(reverseSeats);
+
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00",13.00));
+        priceSeats.add(theatre.new Seat("A00",13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+
+        System.out.println("\npriceSeats");
+        printList(priceSeats);
+    }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for(Theatre.Seat seat: list) {
+            System.out.print(" " + seat.getSeatNumber() + " $" + seat.getPrice());
+        }
+        System.out.println();
+        System.out.print("===========================================");;
+        System.out.println("===========================================");
+    }
+}
