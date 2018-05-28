8b745f7b10d6952e8d31db530f3f0ea5f42ec632
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-05-29 00:10:49 +0530

    Started 110 HashSet equals and hashCode

---
+++ 13-Java_Collections/110-HashSet_equals_and_hashCode/src/com/rajatsachdeva/HeavenlyBody.java
@@ -0,0 +1,40 @@
+package com.rajatsachdeva;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<HeavenlyBody>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        // To return the copy of set rather than original
+        return new HashSet<>(this.satellites);
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
+
+    @Override
+    public String toString() {
+        return name + "\t:\t" + orbitalPeriod;
+    }
+
+
+}
--- 13-Java_Collections/110-HashSet_equals_and_hashCode/src/com/rajatsachdeva/Main.java
+++ 13-Java_Collections/110-HashSet_equals_and_hashCode/src/com/rajatsachdeva/Main.java
@@ -1,8 +1,116 @@
 package com.rajatsachdeva;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+// Program to model the bodies of Solar System
 public class Main {
+    // Store the Heavenly Bodies in a Map
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
 
     public static void main(String[] args) {
-	// write your code here
+
+        HeavenlyBody temp = new HeavenlyBody("Mercury", 88);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Venus", 225);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Earth", 365);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        HeavenlyBody tempMoon = new HeavenlyBody("Moon", 27);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is Earth
+
+        temp = new HeavenlyBody("Mars", 687);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        tempMoon = new HeavenlyBody("Deimos", 1.3);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is Mars
+
+        tempMoon = new HeavenlyBody("Phobos", 0.3);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is Mars
+
+        temp = new HeavenlyBody("Jupiter", 4332);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        tempMoon = new HeavenlyBody("Io", 1.8);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is Jupiter
+
+        tempMoon = new HeavenlyBody("Europa", 3.5);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is Jupiter
+
+        tempMoon = new HeavenlyBody("Ganymede", 7.1);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is Jupiter
+
+        tempMoon = new HeavenlyBody("Callisto", 16.7);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is Jupiter
+
+        temp = new HeavenlyBody("Saturn", 10759);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Uranus", 30660);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Neptune", 165);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Pluto", 248);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        System.out.println("Planets");
+        for (HeavenlyBody planet: planets) {
+            System.out.println("\t" + planet.toString());
+        }
+
+        HeavenlyBody body = solarSystem.get("Jupiter");
+        System.out.println("\nMoons of " + body.getName());
+        for(HeavenlyBody jupiterMoon: body.getSatellites()){
+            System.out.println("\t" + jupiterMoon.getName());
+        }
+
+        // Union of Moons
+        Set<HeavenlyBody> moons = new HashSet<>();
+        for(HeavenlyBody planet: planets) {
+            moons.addAll(planet.getSatellites());
+        }
+
+        System.out.println("\nAll Moons");
+        for (HeavenlyBody moon: moons) {
+            System.out.println("\t" + moon.getName());
+        }
+
+        HeavenlyBody pluto = new HeavenlyBody("Pluto", 842);
+        planets.add(pluto);
+
+        System.out.println("\nPlanets");
+        for(HeavenlyBody planet: planets) {
+            System.out.println("\t" + planet.toString());
+        }
+
+        // Base Object from which all objects inherit from in Java
+        Object o = new Object();
+        o.equals(o);
+        "pluto".equals("");
+
     }
 }
