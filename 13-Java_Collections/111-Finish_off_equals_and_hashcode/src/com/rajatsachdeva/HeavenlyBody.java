59a8b48137501302d61cdec0d7877add0c3daf82
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-05-30 23:52:49 +0530

    Started Finish off equals and hashcode

---
+++ 13-Java_Collections/111-Finish_off_equals_and_hashcode/src/com/rajatsachdeva/HeavenlyBody.java
@@ -0,0 +1,57 @@
+package com.rajatsachdeva;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<HeavenlyBody>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        // To return the copy of set rather than original
+        return new HashSet<>(this.satellites);
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
+
+    @Override
+    public String toString() {
+        return name + "\t:\t" + orbitalPeriod;
+    }
+
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+
+        System.out.println("obj.getClass() is " + obj.getClass());
+        System.out.println("this.getClass() is " + this.getClass());
+
+        if(obj == null || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+
+        String objName = ((HeavenlyBody) obj).getName();
+        return this.name.equals(objName);
+    }
+}
