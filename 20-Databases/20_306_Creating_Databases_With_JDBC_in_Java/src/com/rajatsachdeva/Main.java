07eb1b1648c3c250050520697a18c66992dd00ae
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-06-10 01:49:39 +0530

    Completed 305 Creating db with JDBC

--- 20-Databases/20_306_Creating_Databases_With_JDBC_in_Java/src/com/rajatsachdeva/Main.java
+++ 20-Databases/20_306_Creating_Databases_With_JDBC_in_Java/src/com/rajatsachdeva/Main.java
@@ -10,6 +10,12 @@ import java.sql.Statement;
 public class Main {
 
     public static void main(String[] args) {
+        /*try (Connection conn = DriverManager.getConnection
+                ("jdbc:sqlite:/Users/rohanrajat/Documents" +
+                        "/Java/Udemy/CompleteJavaMasterClass/Complete_Java_Masterclass" +
+                        "/20-Databases/20_306_Creating_Databases_With_JDBC_in_Java/testjava.db");
+             Statement statement = conn.createStatement();) {*/
+
         try {
             /*
             OLD ways
@@ -22,12 +28,11 @@ public class Main {
                     ("jdbc:sqlite:/Users/rohanrajat/Documents" +
                             "/Java/Udemy/CompleteJavaMasterClass/Complete_Java_Masterclass" +
                             "/20-Databases/20_306_Creating_Databases_With_JDBC_in_Java/testjava.db");
-
             Statement statement = conn.createStatement();
             statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
 
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
         }
+        }
     }
-}
