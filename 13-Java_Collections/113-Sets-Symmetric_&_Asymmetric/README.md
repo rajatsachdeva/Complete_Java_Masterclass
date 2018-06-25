24b11c291046bd994a6729f7ef0ea167d28b202c
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-06-25 21:53:04 +0530

    Completed 113

--- 13-Java_Collections/113-Sets-Symmetric_&_Asymmetric/README.md
+++ 13-Java_Collections/113-Sets-Symmetric_&_Asymmetric/README.md
@@ -1,3 +1,6 @@
 # Lecture 113 - Sets - Symmetric & Asymmetric
 
 [_Link_](https://docs.oracle.com/javase/tutorial/collections/interfaces/set.html)
+
+
+Set Differences are asymmetric 
---
+++ 13-Java_Collections/113-Sets-Symmetric_&_Asymmetric/src/com/rajatsachdeva/SetMain.java
@@ -0,0 +1,55 @@
+package com.rajatsachdeva;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMain {
+    public static void main(String[] args) {
+        Set<Integer> squares = new HashSet<>();
+        Set<Integer> cubes = new HashSet<>();
+
+        for (int i = 1; i <= 100; i++) {
+            squares.add(i * i);
+            cubes.add(i * i * i);
+        }
+
+        System.out.println("There are " + squares.size() + " squares and " + cubes.size() + " cubes.");
+
+        // Bulk operations are destructive i.e. they tend to modify the input they work on
+        // so we will using a copy of the inputs
+
+        // Union
+        Set<Integer> union = new HashSet<>(squares);
+        union.addAll(cubes);
+        System.out.println("Union contains " + union.size() + " elements.");
+
+        // Intersection
+        Set<Integer> intersection = new HashSet<>(squares);
+        intersection.retainAll(cubes);
+        System.out.println("Intersection contains " + intersection.size() + " elements.");
+        for (int i : intersection) {
+            System.out.println(i + " is the square of " + Math.sqrt(i) + " and cube of " + Math.cbrt(i));
+        }
+
+        System.out.println();
+        Set<String> words = new HashSet<>();
+        String sentence = "one day in the year of the fox";
+        String[] arrayWords = sentence.split(" ");
+        words.addAll(Arrays.asList(arrayWords));
+
+        for(String s: words) {
+            System.out.println(s);
+        }
+
+        Set<String> nature = new HashSet<>();
+        Set<String> divine = new HashSet<>();
+        String[] natureWords = {"all", "nature", "is", "but", "art", "unknown", "to", "thee"};
+        nature.addAll(Arrays.asList(natureWords));
+
+        String[] divineWords = {"to", "err", "is", "human", "to", "forgive", "divine"};
+        divine.addAll(Arrays.asList(divineWords));
+
+        // Set Differences are asymmetric
+    }
+}
