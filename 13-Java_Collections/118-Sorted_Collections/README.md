1320a451dbe5b30651a1502f71f03e88fae511a3
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-06-27 01:03:34 +0530

    Started 118 - In progress

--- 13-Java_Collections/118-Sorted_Collections/README.md
+++ 13-Java_Collections/118-Sorted_Collections/README.md
@@ -1,1 +1,7 @@
 # Lecture 118 Sorted Collections
+
+Linked HashSet and Linked HashMap sorted versions for hashset and hashmap
+
+Ordering of items in hashset or hashmap is defined as chaotic in oracle documentation
+
+
---
+++ 13-Java_Collections/118-Sorted_Collections/src/com/rajatsachdeva/StockItem.java
@@ -0,0 +1,54 @@
+package com.rajatsachdeva;
+
+public class StockItem implements Comparable<StockItem>{
+    private final String name;
+    private double price;
+    private int quantityStock = 0; // can be initialized later as well
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0; // we don't need this twice but we do either way
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantityStock() {
+        return quantityStock;
+    }
+
+    public void setPrice(double price) {
+        if(price > 0.0) {
+            this.price = price;
+        }
+    }
+
+    public void adjustStock(int quantity) {
+        int newQuantity = this.quantityStock + quantity;
+
+        if(newQuantity > 0.0) {
+            this.quantityStock = newQuantity;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("Entering StockItem.equals()");
+        if(obj == this) {
+            return true;
+        }
+
+        if(obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        String objName = ((StockItem) obj).getName();
+        return this.getName().equals(objName);
+    }
+}
