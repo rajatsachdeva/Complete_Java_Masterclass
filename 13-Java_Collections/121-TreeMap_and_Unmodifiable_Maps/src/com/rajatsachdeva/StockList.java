0ddfb9d1f36e27d8299c9d3ca5b0edd7c27e4ed0
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-06-29 01:09:42 +0530

    Started 121

---
+++ 13-Java_Collections/121-TreeMap_and_Unmodifiable_Maps/src/com/rajatsachdeva/StockList.java
@@ -0,0 +1,63 @@
+package com.rajatsachdeva;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new LinkedHashMap<>();
+    }
+
+    public int addStock(StockItem item) {
+        if (item != null) {
+            // check if list already have this item
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            // if item already present, just increase/adjust the quantity of the item
+            if (inStock != item) {
+                item.adjustStock(inStock.quantityInStock());
+            }
+
+            list.put(item.getName(), item);
+            return item.quantityInStock();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity) {
+        StockItem inStock = list.getOrDefault(item, null);
+
+        if ((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0)) {
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+    public StockItem get(String key) {
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> Items() {
+        // returns read-only Map
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\nStock List\n";
+        double totalCost = 0.0;
+
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            StockItem stockItem = item.getValue();
+            double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+
+            s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
+            s = s + String.format("%.2f", itemValue) + "\n";
+            totalCost += itemValue;
+        }
+        return s + "Total stock value: " + String.format("%.2f", totalCost);
+    }
+}
