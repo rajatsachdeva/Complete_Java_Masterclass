6cd347dae9251b16312b45c60c84b0ceafc02485
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-06-29 01:04:30 +0530

    Started 120 - In progress

--- 13-Java_Collections/120-Add_a_Basket/src/com/rajatsachdeva/StockList.java
+++ 13-Java_Collections/120-Add_a_Basket/src/com/rajatsachdeva/StockList.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class StockList {
     private final Map<String, StockItem> list;
 
-    public StockList(Map<String, StockItem> list) {
+    public StockList() {
         this.list = new HashMap<>();
     }
 
@@ -55,9 +55,9 @@ public class StockList {
             double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
 
             s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
-            s = s + itemValue + "\n";
+            s = s + String.format("%.2f", itemValue) + "\n";
             totalCost += itemValue;
         }
-        return s + " Total stock value: " + totalCost;
+        return s + "Total stock value: " + String.format("%.2f", totalCost);
     }
 }
