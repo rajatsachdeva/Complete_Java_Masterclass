a21220d847f8088a0bc9c1992559d0b4d29a7617
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-07-05 23:35:53 +0530

    Started 121 - In progress

--- 13-Java_Collections/121-TreeMap_and_Unmodifiable_Maps/src/com/rajatsachdeva/Basket.java
+++ 13-Java_Collections/121-TreeMap_and_Unmodifiable_Maps/src/com/rajatsachdeva/Basket.java
@@ -28,7 +28,8 @@ public class Basket {
 
     @Override
     public String toString() {
-        String s = "\nShopping basket " + name + " contains " + list.size() + " items\n";
+        String s = "\nShopping basket " + name + " contains " + list.size() + ((list.size() == 1) ? " item" : " " +
+                "items") + "\n";
         double totalCost = 0.0;
         for (Map.Entry<StockItem, Integer> item : list.entrySet()) {
             s = s + item.getKey() + ". " + item.getValue() + " purchased\n";
