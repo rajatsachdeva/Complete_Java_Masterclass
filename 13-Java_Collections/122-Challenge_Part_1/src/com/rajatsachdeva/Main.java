5e167961e483e378c41b14eb701aa53707cd9fd9
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-07-07 00:48:49 +0530

    Started 122

---
+++ 13-Java_Collections/122-Challenge_Part_1/src/com/rajatsachdeva/Main.java
@@ -0,0 +1,97 @@
+package com.rajatsachdeva;
+
+import java.util.Map;
+
+public class Main {
+
+    private static StockList stockList = new StockList();
+
+    public static void main(String[] args) {
+        StockItem temp = new StockItem("bread", 0.86, 100);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cake", 1.10, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("car", 12.50, 2);
+        stockList.addStock(temp);
+
+        temp = new StockItem("chair", 62.0, 10);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.50, 200);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.45, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("door", 72.95, 4);
+        stockList.addStock(temp);
+
+        temp = new StockItem("juice", 2.5, 36);
+        stockList.addStock(temp);
+
+        temp = new StockItem("phone", 96.99, 35);
+        stockList.addStock(temp);
+
+        temp = new StockItem("towel", 2.40, 80);
+        stockList.addStock(temp);
+
+        temp = new StockItem("vase", 8.76, 40);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
+        System.out.println();
+
+        for (String s : stockList.Items().keySet()) {
+            System.out.println(s);
+        }
+
+        Basket rajatsBasket = new Basket("Rajat");
+
+        sellItem(rajatsBasket, "car", 1);
+        System.out.println(rajatsBasket);
+
+        sellItem(rajatsBasket, "car", 1);
+        System.out.println(rajatsBasket);
+
+        if (sellItem(rajatsBasket, "car", 1) != 1) {
+            System.out.println("There are no more cars to sell");
+        }
+        sellItem(rajatsBasket, "item that does not exists", 1);
+        System.out.println(rajatsBasket);
+
+        sellItem(rajatsBasket, "juice", 4);
+        sellItem(rajatsBasket, "cup", 12);
+        sellItem(rajatsBasket, "bread", 1);
+        System.out.println(rajatsBasket);
+
+        System.out.println(stockList);
+
+        // Following generates an exception as we are adding element in an Unmodifiable Map
+//        temp = new StockItem("pen", 1.12);
+//        stockList.Items().put(temp.getName(), temp);
+        stockList.Items().get("car").adjustStock(2000);
+        stockList.get("car").adjustStock(-1000);
+        System.out.println(stockList);
+
+        for (Map.Entry<String, Double> price : stockList.PriceList().entrySet()) {
+            System.out.println(price.getKey() + " costs " + price.getValue());
+        }
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+        // retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if (stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+
+        if (stockList.sellStock(item, quantity) != 0) {
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0; // didn't have sufficient quantity to sell
+    }
+}
