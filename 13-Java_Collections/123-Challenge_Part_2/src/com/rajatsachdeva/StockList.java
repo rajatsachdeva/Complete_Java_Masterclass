faef740edebe3a0955bdc4b631a0ea44e26a827e
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-08-05 20:12:29 +0530

    Started 123

---
+++ 13-Java_Collections/123-Challenge_Part_2/src/com/rajatsachdeva/StockList.java
@@ -0,0 +1,71 @@
+package com.rajatsachdeva;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new LinkedHashMap<>();
+    }
+
+    public int addStock(StockItem item) {
+        if (item != null) {
+            // check if list already have this item
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            // if item already present, just increase/adjust the quantity of the item
+            if (inStock != item) {
+                item.adjustStock(inStock.availableQuantity());
+            }
+
+            list.put(item.getName(), item);
+            return item.availableQuantity();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity) {
+        StockItem inStock = list.getOrDefault(item, null);
+
+        if ((inStock != null) && (inStock.availableQuantity() >= quantity) && (quantity > 0)) {
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+    public StockItem get(String key) {
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> Items() {
+        // returns read-only Map
+        return Collections.unmodifiableMap(list);
+    }
+
+    public Map<String, Double> PriceList() {
+        Map<String, Double> prices = new LinkedHashMap<>();
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            prices.put(item.getKey(), item.getValue().getPrice());
+        }
+        return Collections.unmodifiableMap(prices);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\nStock List\n";
+        double totalCost = 0.0;
+
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            StockItem stockItem = item.getValue();
+            double itemValue = stockItem.getPrice() * stockItem.availableQuantity();
+
+            s = s + stockItem + ". There are " + stockItem.availableQuantity() + " in stock. Value of items: ";
+            s = s + String.format("%.2f", itemValue) + "\n";
+            totalCost += itemValue;
+        }
+        return s + "Total stock value: " + String.format("%.2f", totalCost);
+    }
+}
