9e58498808b4692b054dbb7101839ffcb8e323d9
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-08-09 00:14:34 +0530

    Started 158

--- 14-JavaFX/158-Code_Vs_FXml/src/sample/Main.java
+++ 14-JavaFX/158-Code_Vs_FXml/src/sample/Main.java
@@ -2,8 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +17,21 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+
+        // Equivalent of using fxml, achieved using code
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setHgap(10);
+//        root.setVgap(10);
+
+        primaryStage.setTitle("Hello JavaFX!");
         primaryStage.setScene(new Scene(root, 300, 275));
+
+//        Label greeting = new Label("Welcome to JavaFx !");
+//        greeting.setTextFill(Color.GREEN);
+//        greeting.setFont(Font.font("Courier", FontWeight.NORMAL, 20));
+//        root.getChildren().add(greeting);
+
         primaryStage.show();
     }
 
