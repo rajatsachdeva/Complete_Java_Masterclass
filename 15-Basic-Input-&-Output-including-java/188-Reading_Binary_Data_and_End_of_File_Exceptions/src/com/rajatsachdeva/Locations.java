dff19406bcb605295062dbd65ff54f0b5ed6c679
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2018-10-17 23:36:26 +0530

    Started 188

---
+++ 15-Basic-Input-&-Output-including-java/188-Reading_Binary_Data_and_End_of_File_Exceptions/src/com/rajatsachdeva/Locations.java
@@ -0,0 +1,128 @@
+package com.rajatsachdeva;
+
+import java.io.*;
+import java.util.*;
+
+public class Locations implements Map<Integer, Location> {
+    private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
+
+    public static void main(String[] args) throws IOException {
+        try (DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations" +
+                ".dat")))) {
+            for (Location location : locations.values()) {
+                locFile.writeInt(location.getLocationID());
+                locFile.writeUTF(location.getDescription()); // used to write string
+                System.out.println("Writing Location: " + location.getLocationID() + " : " + location.getDescription());
+                System.out.println("Writing " + (location.getExits().size() - 1) + " exits.");
+                locFile.writeInt(location.getExits().size() - 1);
+
+                for (String direction : location.getExits().keySet()) {
+                    if (!direction.equalsIgnoreCase("Q")) {
+                        System.out.println("\t\t" + direction + "," + location.getExits().get(direction));
+                        locFile.writeUTF(direction);
+                        locFile.writeInt(location.getExits().get(direction));
+                    }
+                }
+            }
+        }
+    }
+
+    // static initialization block
+    // This block will be loaded only once , when the Locations class is loaded
+    static {
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("locations_big.txt")))) {
+            scanner.useDelimiter(",");
+
+            while(scanner.hasNextLine()) {
+                int loc = scanner.nextInt();
+                scanner.skip(scanner.delimiter());
+                String description = scanner.nextLine();
+                System.out.println("Imported: " + loc + " : " + description);
+
+                Map<String, Integer> tempExit = new HashMap<String, Integer>();
+                locations.put(loc, new Location(loc, description, tempExit));
+
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        // read the exits from directions.txt
+        try (BufferedReader dirFile = new BufferedReader(new FileReader("directions_big.txt"))){
+            String input;
+            while ((input = dirFile.readLine()) != null) {
+
+                String[] data = input.split(",");
+                int loc = Integer.parseInt(data[0]);
+                String direction = data[1];
+                int destination = Integer.parseInt(data[2]);
+
+                System.out.println(loc + ": " + direction + ": " + destination);
+                Location location = locations.get(loc);
+                location.addExit(direction, destination);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+}
--- 15-Basic-Input-&-Output-including-java/188-Reading_Binary_Data_and_End_of_File_Exceptions/src/com/rajatsachdeva/Main.java
+++ 15-Basic-Input-&-Output-including-java/188-Reading_Binary_Data_and_End_of_File_Exceptions/src/com/rajatsachdeva/Main.java
@@ -1,8 +1,76 @@
 package com.rajatsachdeva;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Lecture 106 Adventure Game Challenge
+ * ====================================
+ *
+ * Change the program to allow players to type full words, or phrases, then move to the
+ * correct location based upon their input.
+ * The player should be able to type commands such as "Go West", "run South", or just "East"
+ * and the program with move to the appropriate location if there is one. As at present, an
+ * attempt to move in an invalid direction should print a message and remain in the same place.
+ *
+ * Single Letter commands (N, W, S, E, Q) should still be available
+ */
+
 public class Main {
 
+    private static Locations locations = new Locations();
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        /**
+         *          <->          5 Forest            [ ^North ]
+         *           |              |
+         *        2 Hill   <-     1 Road    <->     3 Building
+         *           |              |
+         *          <-         4 Valley
+         */
+
+        Map<String, String> vocabulary = new HashMap<String, String>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+
+        int loc = 1;
+        while(true) {
+            // will generate a null pointer exception as value doesn't exists for the case of invalid key
+            System.out.println(locations.get(loc).getDescription());
+
+            if (loc == 0) {
+                break;
+            }
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for (String exit: exits.keySet()) {
+                System.out.print(exit +", " );
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if (direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for (String word : words) {
+                    if (vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
+                System.out.println("You cannot go in that direction");
+            }
+        }
     }
 }
